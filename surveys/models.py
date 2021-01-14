3d9971df6a287f0c77ea1b7c5a2e575641b3e0b8
kolaqsq <kolaqsq1707@gmail.com>
2021-01-14 23:39:22 +0300

    added models to surveys app

--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Survey(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    survey_title = models.CharField('Название анкеты', max_length=200)
+    creation_date = models.DateTimeField('Дата публикации')
+    is_open = models.BooleanField('Открыто для заполнения')
+
+
+class QuestionType(models.Model):
+    type_name = models.CharField('Тип вопроса', max_length=50)
+
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question_type = models.ForeignKey(QuestionType, on_delete=models.DO_NOTHING)
+    question_text = models.TextField('Вопрос', max_length=500)
