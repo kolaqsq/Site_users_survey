c0b7885f49d86fc060b17225671518e629e408c3
kolaqsq <kolaqsq1707@gmail.com>
2021-01-16 11:08:52 +0300

    Added test build

--- Site_users_survey/settings.py
+++ Site_users_survey/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(__file__)
 SECRET_KEY = 'ifhrsbxy!d0c%*x(gr%cjr4)_f81r^$f7_$fqs9b2j6%xdeem@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
