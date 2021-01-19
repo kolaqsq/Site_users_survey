d8299180c223cb8014a6e644714e93a804989ff7
kolaqsq <kolaqsq1707@gmail.com>
2021-01-20 01:32:23 +0300

    updated answers app

--- answers/admin.py
+++ answers/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+from django.db import connection
 
-# Register your models here.
+from answers.models import Surveyee, SurveyeeSurvey, QuestionOption, Answer
+from surveys.models import Survey
+
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 0
+
+
+class SurveyeeSurveyAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+    def get_list_display(self, request):
+        if request.user.is_superuser:
+            return ['creator', 'survey_title', 'name', 'surname']
+        else:
+            return ['survey_title', 'name', 'surname']
+
+    def get_list_filter(self, request):
+        if request.user.is_superuser:
+            return ['creator', 'survey']
+        else:
+            return ['survey']
+
+    def get_queryset(self, request):
+        query = SurveyeeSurvey.objects.filter(creator=request.user)
+        if request.user.is_superuser:
+            query = SurveyeeSurvey.objects.all()
+        return query
+
+    def survey_title(self, obj):
+        return obj.survey.survey_title
+
+    survey_title.short_description = 'Анкета'
+
+    def name(self, obj):
+        return obj.surveyee.name
+
+    name.short_description = 'Имя'
+
+    def surname(self, obj):
+        return obj.surveyee.surname
+
+    surname.short_description = 'Фамилия'
+
+
+class SurveyeeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname']
+    list_filter = ['name', 'surname']
+    search_fields = ['name', 'surname']
+
+
+admin.site.register(Surveyee, SurveyeeAdmin)
+admin.site.register(SurveyeeSurvey, SurveyeeSurveyAdmin)
--- answers/models.py
+++ answers/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from surveys.models import Survey, Question, OptionChoice
@@ -9,7 +10,8 @@ class Surveyee(models.Model):
     email = models.CharField('E-mail', max_length=100)
 
     def __str__(self):
-        return self.name, self.surname
+        template = '{0.name} {0.surname}'
+        return template.format(self)
 
     class Meta:
         verbose_name = 'Анкетируемый'
@@ -17,16 +19,49 @@ class Surveyee(models.Model):
 
 
 class SurveyeeSurvey(models.Model):
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Создатель', editable=False)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, verbose_name='Анкета')
     surveyee = models.ForeignKey(Surveyee, on_delete=models.CASCADE, verbose_name='Анкетируемый')
+
+    def __str__(self):
+        template = 'Набор ответов №{0.id}. {0.surveyee}, {0.survey}'
+        return template.format(self)
+
+    class Meta:
+        verbose_name = 'Набор ответов'
+        verbose_name_plural = 'Наборы ответов'
 
 
 class QuestionOption(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    option_choice = models.ForeignKey(OptionChoice, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Вопрос')
+    option_choice = models.ForeignKey(OptionChoice, on_delete=models.CASCADE, verbose_name='Ответ')
+
+    def __str__(self):
+        template = '{0.option_choice.choice_name}'
+        return template.format(self)
 
 
 class Answer(models.Model):
-    surveyee = models.ForeignKey(Surveyee, on_delete=models.CASCADE)
-    question_option = models.ForeignKey(QuestionOption, on_delete=models.CASCADE)
-    answer_text = models.TextField(max_length=1000)
+    answer_set = models.ForeignKey(SurveyeeSurvey, on_delete=models.CASCADE, verbose_name='Набор ответов')
+    surveyee = models.ForeignKey(Surveyee, on_delete=models.CASCADE, verbose_name='Анкетируемый', editable=False)
+    question_option = models.ForeignKey(QuestionOption, on_delete=models.CASCADE, verbose_name='Ответ')
+    answer_text = models.TextField(max_length=1000, blank=True, null=True, verbose_name='Текст ответа')
+
+    def __str__(self):
+        template = 'Ответ №{0.id}. {0.surveyee}, {0.answer_set.survey}'
+        return template.format(self)
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+# insert into std_950.answers_answer (answer_text) values ('20');
+# insert into std_950.answers_answer (answer_text) values ('44');
+# insert into std_950.answers_answer (answer_text) values ('Согласен');
+# insert into std_950.answers_answer (answer_text) values ('Согласен');
+# insert into std_950.answers_answer (answer_text) values ('Не могу решить');
+# insert into std_950.answers_answer (answer_text) values ('Не согласен');
+# insert into std_950.answers_answer (answer_text) values ('Согласен');
+# insert into std_950.answers_answer (answer_text) values ('Частично не соответствует');
+# insert into std_950.answers_answer (answer_text) values ('2');
+# insert into std_950.answers_answer (answer_text) values ('б');
